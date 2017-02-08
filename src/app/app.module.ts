3a4884a48eaba6c7ceb7f12d68a67f36aa578a96
Borys Ponomarenko <borysp@backbase.com>
2017-02-08 17:00:20 +0100

    chore: Disable AoT compilation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+// import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { ModalModule } from 'ng2-bootstrap/modal';
 import { AlertModule } from 'ng2-bootstrap/alert';
 
