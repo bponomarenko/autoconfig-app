a7df628a16fe6cdb782bb2cb08abe19d5ad0ac24
Borys Ponomarenko <borysp@backbase.com>
2017-03-03 12:17:41 +0100

    chore: Add windows build configuration

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 set -ev
 
 # Build and deploy web version only on osx
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+if [[ $TARGET_OS == 'osx' ]]; then
   npm run build:web -- -bh /autoconfig-app/
   ngh --dir=dist/web --repo=https://$GH_TOKEN@github.com/bponomarenko/autoconfig-app.git --name="Borys Ponomarenko" --email=bponomarenko@gmail.com
 fi
@@ -11,11 +11,16 @@ fi
 npm run build:web4app
 
 # Build for OSX
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+if [[ $TARGET_OS == 'osx' ]]; then
   npm run build -- -m -p onTagOrDraft
 fi
 
 # Build for Linux
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+if [[ $TARGET_OS == 'linux' ]]; then
   npm run build -- -l -p onTagOrDraft
+fi
+
+# Build for Windows
+if [[ $TARGET_OS == 'win' ]]; then
+  npm run build -- -w -p onTagOrDraft
 fi
--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ set -ev
 npm i @angular/cli -g --silent
 npm i angular-cli-ghpages -g --silent
 
-# if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-#   brew update
-#   brew install gnu-tar graphicsmagick xz
-# fi
+if [[ $TARGET_OS == 'win' ]]; then
+  brew update
+  brew install gnu-tar graphicsmagick xz
+fi
