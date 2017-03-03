8d6ebd4931a64af54bcbb3d9148143ea0dddebfc
Borys Ponomarenko <borysp@backbase.com>
2017-03-04 00:59:05 +0100

    chore: Try to build win app on linux machine

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,15 @@ npm i @angular/cli -g --silent
 npm i angular-cli-ghpages -g --silent
 
 if [[ $TARGET_OS == 'win' ]]; then
-  brew update
-  brew install wine --without-x11
-  brew install mono
+  # brew update
+  # brew install wine --without-x11
+  # brew install mono
+
+  sudo add-apt-repository ppa:ubuntu-wine/ppa -y
+  sudo apt-get update
+  sudo apt-get install --no-install-recommends -y wine1.8
+
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+  echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+  sudo apt-get install --no-install-recommends -y mono-devel ca-certificates-mono
 fi
