7e20d7ef67a81cdc96cf128ad5017cd6c4f3b4be
Borys Ponomarenko <borysp@backbase.com>
2017-03-03 13:09:50 +0100

    fix: Correct build configuration

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,5 +6,6 @@ npm i angular-cli-ghpages -g --silent
 
 if [[ $TARGET_OS == 'win' ]]; then
   brew update
-  brew install gnu-tar graphicsmagick xz
+  brew install wine --without-x11
+  brew install mono
 fi
