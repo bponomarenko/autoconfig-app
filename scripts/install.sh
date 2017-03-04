357fe04db45f6a467734b51a257bcbb20e60d9d0
Borys Ponomarenko <borysp@backbase.com>
2017-03-04 16:12:27 +0100

    chore: One more try to fix win build

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,9 +9,12 @@ if [[ $TARGET_OS == 'win' ]]; then
   # brew install wine --without-x11
   # brew install mono
 
+  sudo dpkg --configure -a
+  sudo apt-get -f install
+
   sudo add-apt-repository ppa:ubuntu-wine/ppa -y
+  sudo dpkg --add-architecture i386
   sudo apt-get update
-  sudo apt-get install libcheese-gtk23 libcheese7
   sudo apt-get install --no-install-recommends -y wine1.8
 
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
