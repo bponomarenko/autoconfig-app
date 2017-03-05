834e50708e519228ac08456ccc1335b26ac94c67
Borys Ponomarenko <borysp@backbase.com>
2017-03-05 12:05:29 +0100

    chore: Disable win build for now

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,9 +9,9 @@ if [[ $TARGET_OS == 'win' ]]; then
   # brew install wine --without-x11
   # brew install mono
 
+  sudo dpkg --add-architecture i386
   sudo add-apt-repository ppa:ubuntu-wine/ppa -y
   sudo apt-get update
-  sudo dpkg --add-architecture i386
   sudo apt-get install --no-install-recommends -y wine1.8
 
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
