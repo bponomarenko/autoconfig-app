47a4ada749b54f5b834028d49984e1541f13d4bd
Borys Ponomarenko <borysp@backbase.com>
2017-03-05 15:00:47 +0100

    chore: Try to build win once again

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,11 +10,15 @@ if [[ $TARGET_OS == 'win' ]]; then
   # brew install mono
 
   sudo dpkg --add-architecture i386
+
+  # Install wine
   sudo add-apt-repository ppa:ubuntu-wine/ppa -y
   sudo apt-get update
   sudo apt-get install --no-install-recommends -y wine1.8
 
+  # Install mono
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
   echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+  sudo apt-get update
   sudo apt-get install --no-install-recommends -y mono-devel ca-certificates-mono
 fi
