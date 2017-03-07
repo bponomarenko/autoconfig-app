12fb1fa213f14dec468fca3c0eaf416fdf73af5b
Borys Ponomarenko <borysp@backbase.com>
2017-03-07 17:23:44 +0100

    chore: Test win build

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,7 +9,7 @@ if [[ $TARGET_OS == 'win' ]]; then
   export LD_LIBRARY_PATH
 
   sudo dpkg --add-architecture i386
-  sudo apt-add-repository ppa:ubuntu-toolchain-r/test
+  sudo apt-add-repository ppa:ubuntu-toolchain-r/test -y
   sudo apt-get update
   sudo apt-get install gcc-4.9 g++-4.9 libstdc++6
 
