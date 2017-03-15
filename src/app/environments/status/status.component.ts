54e24035cae3687596b261946e5673b9a00b46f1
Borys Ponomarenko <borysp@backbase.com>
2017-03-15 10:33:58 +0100

    feat: Improve environment statuses badge

--- src/app/environments/status/status.component.ts
+++ src/app/environments/status/status.component.ts
@@ -15,8 +15,12 @@ export class StatusComponent {
     switch(this.status) {
       case 5:
         return 'badge-success';
+      case 3:
+        return 'badge-primary';
       case 2:
         return `badge-warning`;
+      case 1:
+        return 'badge-info';
       case -1:
         return `badge-danger`;
       default:
@@ -28,13 +32,22 @@ export class StatusComponent {
     let text: string;
     switch(this.status) {
       case 5:
-        text = `OK`;
+        text = `Configured`;
+        break;
+      case 4:
+        text = `Destroyed`;
+        break;
+      case 3:
+        text = `Provisioned, but not configured`;
         break;
       case 2:
-        text = `Provisioning`;
+        text = `Provisioning in progress`;
+        break;
+      case 1:
+        text = `Queued`;
         break;
       case -1:
-        text = `Deleted`;
+        text = `Failed`;
         break;
       default:
         text = `Unknown status`;
